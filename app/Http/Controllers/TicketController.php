505f66f75f0fee8f588a54cda88a892230058384
Mohamed Adhuham <hey@adhham.com>
2025-12-03 10:02:58 +0500

    create submission form

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -2,12 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Enums\Severity;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class TicketController extends Controller
 {
     public function index()
     {
-        return view('tickets.index');
+        $categories = Category::all();   
+        $severityLevels = Severity::cases();
+
+        return view('tickets.index', [
+            'categories' => $categories,
+            'severityLevels' => $severityLevels,
+        ]);
     }
 }
