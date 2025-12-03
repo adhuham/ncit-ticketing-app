abbdb31169eec037aee3db78ac18cbe317331d66
Mohamed Adhuham <hey@adhham.com>
2025-12-03 11:01:35 +0500

    added tests

---
+++ tests/Feature/TicketTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Enums\Severity;
+use App\Models\Category;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class TicketTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * A basic test example.
+     */
+    public function test_can_create_tickets(): void
+    {
+        $user = User::factory()->create();
+
+        foreach (['Access', 'Hardware', 'Software'] as $categoryName) {
+            $category = new Category();
+            $category->name = $categoryName;
+            $category->save();
+        }   
+
+        $response = $this->actingAs($user)
+            ->get('/tickets', [
+                'title' => 'Sample Ticket',
+                'description' => 'This is a sample ticket description.',
+                'category_id' => Category::first()->id,
+                'severity' => Severity::LOW->value,
+            ]);
+
+        $response->assertStatus(200);
+    }
+}
