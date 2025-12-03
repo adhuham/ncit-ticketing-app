bb931c9d125a5f9adde04771267e76bfe554c038
Mohamed Adhuham <hey@adhham.com>
2025-12-03 11:41:58 +0500

    fix + test invalid case

--- tests/Feature/TicketTest.php
+++ tests/Feature/TicketTest.php
@@ -12,22 +12,38 @@ class TicketTest extends TestCase
 {
     use RefreshDatabase;
 
-    /**
-     * A basic test example.
-     */
     public function test_can_create_tickets(): void
     {
         $user = User::factory()->create();
         Category::factory()->create();
 
         $response = $this->actingAs($user)
-            ->get('/tickets', [
+            ->post('/tickets', [
                 'title' => 'Sample Ticket',
                 'description' => 'This is a sample ticket description',
                 'category_id' => Category::first()->id,
                 'severity' => Severity::LOW->value,
             ]);
 
-        $response->assertStatus(200);
+        $response->assertRedirect();
+        $response->assertSessionHasNoErrors();
+    }
+
+    public function test_cannot_create_tickets_without_title_and_description(): void
+    {
+        $user = User::factory()->create();
+        Category::factory()->create();
+
+        $response = $this->actingAs($user)
+            ->post('/tickets', [
+                'title' => null,
+                'description' => null,
+                'category_id' => Category::first()->id,
+                'severity' => Severity::LOW->value,
+            ]);
+
+        $response->assertRedirect();
+        $response->assertSessionHasErrors();
+
     }
 }
